0147cce682c7f9f95cf09e1a9a3c36cd2e48365c
fmontoy <fmontoyalvarez@gmail.com>
2017-05-21 20:43:45 -0500

    Listo

--- WiFiWebClient/NodeMCU_IoT.ino
+++ WiFiWebClient/NodeMCU_IoT.ino
@@ -1,26 +1,24 @@
 //Libraries
 #include "MeanFilterLib.h"
 #include <ESP8266WiFi.h>
-//#include <ArduinoJson.h>
+#include <Time.h>
+#include <ArduinoJson.h>
 
 //Pin I/O Labeling
 #define CurrSensor 0  //IR distance sensor connected to Arduino analog pin 0
-//#define flow 5  //Flow sensor connected to Arduino digital pin 5
-//#define Electrovalve1 1  //Electrovalve (+) connected to Arduino pwm pin 1
-//#define Electrovalve2 2  //Electrovalve (-) connected to Arduino pwm pin 1
-//#define Pump1 3  //Pump (+) connected to Arduino pwm pin 3
-//#define Pump2 4  //Pump (-) connected to Arduino pwm pin 3
-MeanFilter<float> meanFilter(10);
+
+MeanFilter<float> meanFilter(43);
 //Constants
 const unsigned long TPOST = 1000;  //Delay between TWX POST updates, 1000 milliseconds
 const unsigned int sensorCount = 1;  //Number of sensor vars sent to TWX, 1 var
-const char* ssid = "ailet"; //WiFi SSID
-const char* password = "1234567890"; //WiFi Pass
+const char* ssid = "JDONADO"; //WiFi SSID
+const char* password = "12345678"; //WiFi Pass
 const char* host = "molpe.eafit.edu.co";  //TWX Host
-
+const char* host_azure = "neutron.eafit.edu.co";
 //Variables
 //->WiFi Shield Vars
 WiFiClient client;
+WiFiClient sclient;
 //->TWX Vars
 char appKey[] = "98a4eae5-d9cb-484f-aa34-857bfc7e934c"; //API Key from TWX
 char thingName[] = "ailet_thing";  //Name of the thing in TWX
@@ -76,7 +74,59 @@ void WiFiInit() {
   printWifiStatus();
 }
 
-void POST(int sensorCount, char* sensorNames[], float values[]) {
+void POST_Azure(float values[]){
+  String url = "/toymoodle/local/integracion_ayre/ailet_test/request_all.php?";
+  url += "amps";
+  url += "=";
+  url += (values[0]);
+  url += "&";
+  url += "ailetID";
+  url += "=";
+  url += "02";
+  url += "&";
+  url += "device";
+  url += "=";
+  url += "Null";
+  url += "&";
+  url += "level";
+  url += "=";
+  url += "LOW";
+  url += "&";
+  url += "user";
+  url += "=";
+  url += "Felipe";
+  
+  Serial.println("haciendo POST a Azure"); 
+  if(client.connect(host_azure, 80)){
+    Serial.println("conected!!!");
+        client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host_azure + "\r\n" +
+                 "Content-Type: text/html\r\n\r\n");
+    Serial.println("Vamos a ver si esto si imprime.....................");
+    
+    unsigned long timeout = millis();
+    
+    while (client.available() == 0) {
+      if (millis() - timeout > 10000000) {
+        Serial.println(">>> Client Timeout !");
+        client.stop();
+        return;
+      }
+    }
+     while (client.available()) {
+      String line = client.readStringUntil('\r');
+      Serial.println(line);
+    }
+    
+  }
+  
+}
+
+
+
+
+/*void POST_Thingworx(int sensorCount, char* sensorNames[], float values[]) {
+ 
   String url = "/Thingworx/Things/";
   url += thingName;
   url += "/Services/";
@@ -85,25 +135,42 @@ void POST(int sensorCount, char* sensorNames[], float values[]) {
   url += appKey;
   url += "&method=post";
   url += "&x-thingworxsession=true";
-  url += "<";
-  for (int idx = 0; idx < sensorCount; idx++)
-  {
-    url += "&";
-    url += propertyNames[idx];
-    url += "=";
-    url += values[idx];
-  }
-  url += ">";
+  url += "&";
+  url += propertyNames[0];
+  url += "=";
+  url += values[0]-1.0;
+  url += "&";
+  url += "active";
+  url += "=";
+  url += "True";
+  url += "&";
+  url += "address";
+  url += "=";
+  url += "EAFIT";
+  url += "&";
+  url += "ailetID";
+  url += "=";
+  url += "02";
+  url += "&";
+  url += "device";
+  url += "=";
+  url += "Celular";
+  url += "&";
+  url += "level";
+  url += "=";
+  url += "LOW";
+  url += "&";
+  url += "user";
+  url += "=";
+  url += "jarbol";
   // if you get a connection, report back via serial:
   if (client.connect(host, 8080)) {
-    Serial.println("connected");
     // send the HTTP POST request:
     client.print(String("POST ") + url + " HTTP/1.1\r\n" +
                  "Host: " + host + "\r\n" +
                  "Content-Type: text/html\r\n\r\n");
-
     // print the request out
-    Serial.print(String("POST ") + url + " HTTP/1.1\r\n" +
+    Serial.println(String("POST ") + url + " HTTP/1.1\r\n" +
                  "Host: " + host + "\r\n" +
                  "Content-Type: text/html\r\n\r\n");
     unsigned long timeout = millis();
@@ -112,7 +179,8 @@ void POST(int sensorCount, char* sensorNames[], float values[]) {
         Serial.println(">>> Client Timeout !");
         client.stop();
         return;
-      }
+  
+     }
     }
 
     // Read all the lines of the reply from server and print them to Serial
@@ -126,92 +194,12 @@ void POST(int sensorCount, char* sensorNames[], float values[]) {
     Serial.println("the connection could not be established");
     client.stop();
   }
-}
+}*/
 
-//void GET(char* writeNames[]) {
-//  String urlG = "/Thingworx/Things/";
-//  urlG += thingName;
-//  urlG += "/Properties";
-//  urlG += "?appKey=";
-//  urlG += appKey;
-//  urlG += "&x-thingworxsession=true";
-//
-//  // if you get a connection, report back via serial:
-//  if (client.connect(host, 80)) {
-//    Serial.println("connected");
-//    // send the HTTP POST request:
-//    client.print(String("GET ") + urlG + " HTTP/1.1\r\n" +
-//                 "Host: " + host + "\r\n" +
-//                 "Accept: application/json\r\n" +
-//                 "Cache-Control: no-cache\r\n\r\n");
-//
-//    // print the request out
-//    Serial.print(String("GET ") + urlG + " HTTP/1.1\r\n" +
-//                 "Host: " + host + "\r\n" +
-//                 "Accept: application/json\r\n" +
-//                 "Cache-Control: no-cache\r\n\r\n");
-//    unsigned long timeout2 = millis();
-//    while (client.available() == 0) {
-//      if (millis() - timeout2 > 5000) {
-//        Serial.println(">>> Client Timeout !");
-//        client.stop();
-//        return;
-//      }
-//    }
-//
-//    // Read all the lines of the reply from server and print them to Serial
-//    String json = "";
-//    boolean httpBody = false;
-//    while (client.available()) {
-//      String line = client.readStringUntil('\r');
-//      if (!httpBody && line.charAt(1) == '{') {
-//        httpBody = true;
-//      }
-//      if (httpBody) {
-//        json += line;
-//        httpBody = false;
-//      }
-//    }
-//    Serial.println(json);
-//    DynamicJsonBuffer jsonBuffer;
-//    JsonObject& root = jsonBuffer.parseObject(json);
-//
-//    if (root.success()) {
-//      Serial.println("JSON parsing Succeed!");
-//    }
-//    //root["rows"][0]["Write1"].printTo(Serial);
-//    //Serial.println(nestedArray[0]);
-//    Write1 = root["rows"][0][writeNames[0]];
-//    Write2 = root["rows"][0][writeNames[1]];
-//    Serial.print("Write1: ");
-//    Serial.print(Write1);
-//    Serial.print(" Write2: ");
-//    Serial.println(Write2);
-//  }
-//  else {
-//    // kf you didn't get a connection to the server:
-//    Serial.println("the connection could not be established");
-//    client.stop();
-//  }
-//}
 
 
 void setup() {
-  //Pin type definition
-//  pinMode(Pump1, OUTPUT);
-//  pinMode(Pump2, OUTPUT);
-//  pinMode(Electrovalve1, OUTPUT);
-//  pinMode(Electrovalve2, OUTPUT);
-//  pinMode(LED_BUILTIN, OUTPUT);
-//  pinMode(flow, INPUT);
-
-  //Physical output cleaning
-//  digitalWrite(Pump1, HIGH);
-//  digitalWrite(Pump2, HIGH);
-//  digitalWrite(Electrovalve1, HIGH);
-//  digitalWrite(Electrovalve2, HIGH);
-//  digitalWrite(LED_BUILTIN, HIGH);
-
+ 
   //Communications initialization
   Serial.begin(115200);
   WiFiInit();
@@ -221,27 +209,12 @@ void loop() {
   propertyValues[0] = meanFilter.AddValue(analogRead(0));
   //propertyValues[1] = 200;
   if (millis() - lastConnectionTime > TPOST) {
-    POST(sensorCount, propertyNames, propertyValues);
-    //GET(writeNames);
+    POST_Azure(propertyValues);
+    //delay(1000);
+    //POST_Thingworx(sensorCount, propertyNames, propertyValues);
+
     lastConnectionTime = millis();
-//    if (Write1 == 1) {
-//      digitalWrite(Electrovalve1, LOW);
-//      digitalWrite(Electrovalve2, LOW);
-//      digitalWrite(LED_BUILTIN, LOW);
-//    }
-//    else {
-//      digitalWrite(Electrovalve1, HIGH);
-//      digitalWrite(Electrovalve2, HIGH);
-//      digitalWrite(LED_BUILTIN, HIGH);
-//    }
-//    if (Write2 == 1) {
-//      digitalWrite(Pump1, LOW);
-//      digitalWrite(Pump2, LOW);
-//    }
-//    else {
-//      digitalWrite(Pump1, HIGH);
-//      digitalWrite(Pump2, HIGH);
-//    }
+
   }
 }
 
